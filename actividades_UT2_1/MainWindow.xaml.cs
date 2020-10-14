2eba6c845a99df341b45c35c61a2c8db79fc2668
marcos <marcos.ruiz16@iesdoctorbalmis.com>
2020-10-14 10:22:43 +0200

    funciona

---
+++ actividades_UT2_1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace actividades_UT2_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Random aleatorio = new Random();
+        int numAleatorio = aleatorio.Next(0, 100);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(numeroTextBox.Text) > numAleatorio) MessageBox.Show("Te has quedado corto");
+            else if (Convert.ToInt32(numeroTextBox.Text) < numAleatorio) MessageBox.Show("Te has pasado");
+            else MessageBox.Show("¡Acertaste!");
+        }
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            numAleatorio = aleatorio.Next(0,100);
+        }
+    }
+}
